c31a5b3b96b91c315632797296f3ac01ad525859
marioachmad <achmadmario75@gmail.com>
2024-01-13 21:52:34 +0700

    commmit

---
+++ Basic-1/src/basic/pkg1/Basic1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basic.pkg1;
+
+/**
+ *
+ * @author mario
+ */
+public class Basic1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String a = "Hello World"; // String for text, number, etc.
+        int b = 10; // Integer for number.
+        
+        System.out.println(a);
+        System.out.println(b);
+    }   
+}
