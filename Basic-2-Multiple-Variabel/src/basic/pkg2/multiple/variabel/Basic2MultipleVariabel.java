9449cae67dccacc1d103dc517419c52b4a261485
marioachmad <ufik0203@gmail.com>
2024-01-16 17:26:51 +0700

    commit

---
+++ Basic-2-Multiple-Variabel/src/basic/pkg2/multiple/variabel/Basic2MultipleVariabel.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basic.pkg2.multiple.variabel;
+
+/**
+ *
+ * @author mario
+ */
+public class Basic2MultipleVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 2, b = 5, c =  10;
+        String A = "Hello", B = "World";
+        int z = a + b + c;
+        
+        System.out.println("Number = " + a + b + c);
+        System.out.println("Number = " + a + "," + b + "," + c);
+        System.out.println("Count = " +( a + b + c));
+        System.out.println("Word = " + A + B);
+        System.out.println("Z = " + z);
+        
+        // its same like other variable, and its basic of multiple variable
+        
+    }
+    
+}
