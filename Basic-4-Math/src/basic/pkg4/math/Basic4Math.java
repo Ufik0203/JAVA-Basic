615359e7cf91306a2f415604ce7674bbae9493b7
Ufik <ufik0203@gmail.com>
2024-01-20 21:33:25 +0700

    update commit

--- Basic-4-Math/src/basic/pkg4/math/Basic4Math.java
+++ Basic-4-Math/src/basic/pkg4/math/Basic4Math.java
@@ -10,43 +10,41 @@ import java.util.Scanner;
  *
  * @author mario
  */
-
 public class Basic4Math {
 
     /**
      * @param args the command line arguments
      */
-    
     private void undec() {
         Scanner scan = new Scanner(System.in);
-        double a,b;
+        double a, b;
         System.out.print("Masukkan angka ke-1 : ");
         a = scan.nextDouble();
-        
+
         System.out.print("Masukkan angka ke-2 : ");
         b = scan.nextDouble();
-        
+
         double z = (Math.max(a, b));
         System.out.println("Nilai tertinggi = " + z);
     }
-    
+
     private void matharray() {
         Scanner scan = new Scanner(System.in);
         int a;
         System.out.print("Masukkan panjang array : ");
         a = scan.nextInt();
-        
+
         int[] b = new int[a + 1];
         for (int i = 1; i <= a; i++) {
             System.out.print("Masukkan angka ke-" + i + " : ");
             b[i] = scan.nextInt();
         }
-       
+
         System.out.println("\nIsi Array : ");
-        for (int i = 1; i <=a; i++) {
+        for (int i = 1; i <= a; i++) {
             System.out.println("Index ke-" + i + " : " + b[i]);
         }
-         
+
         int max = b[1];
         for (int i = 2; i <= a; i++) {
             if (b[i] > max) {
@@ -55,14 +53,14 @@ public class Basic4Math {
         }
         System.out.print("\n\nNilai tertinggi = " + max + "\n\n");
     }
-    
+
     private static int pangkat(int a) {
         return (int) (Math.sqrt(a));
     }
-    
+
     private void kuadrat() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Masukkan panjang array");
+        System.out.print("Masukkan panjang array : ");
         int a = scan.nextInt();
         int[] b = new int[a + 1];
 
@@ -70,15 +68,16 @@ public class Basic4Math {
             System.out.print("Masukkan nilai ke-" + i + " : ");
             b[i] = scan.nextInt();
         }
-        
+
         System.out.println("\nIsi Array : ");
         for (int i = 1; i <= a; i++) {
             System.out.println("Index ke-" + i + " : " + b[i]);
         }
-        
+
         int sqre = b[1];
-        for (int i = 1; i <= a; i++){
-            if()
+        for (int i = 1; i <= a; i++) {
+            sqre = pangkat(b[i]);
+            System.out.println("Hasil : " + sqre);
         }
     }
 
@@ -86,7 +85,7 @@ public class Basic4Math {
         Basic4Math obj = new Basic4Math();
         Scanner scan = new Scanner(System.in);
         boolean i = true;
-        
+
         System.out.println("===== Declare =====");
         System.out.println(Math.max(10, 20));   // use to find highest value
         System.out.println(Math.min(10, 20));   // use to find lowest value
@@ -109,6 +108,8 @@ public class Basic4Math {
         int z = pangkat(a);
         System.out.print(a + " nilai kuadrat dari : " + z + "\n\n");
 
-        
+        System.out.println("\n\n====== Comb Array ======");
+        obj.kuadrat();
+
     }
 }
