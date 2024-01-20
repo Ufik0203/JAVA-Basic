8190e01cb79b42e14cd18ca133ef8897bc429e9a
Ufik <ufik0203@gmail.com>
2024-01-20 22:15:47 +0700

    update commit

--- Basic-4-Boolean/src/basic/pkg5/pkgboolean/Basic5Boolean.java
+++ Basic-4-Boolean/src/basic/pkg5/pkgboolean/Basic5Boolean.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package basic.pkg4.pkgboolean;
+package basic.pkg5.pkgboolean;
 
 import java.util.Scanner;
 
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author mario
  */
-public class Basic4Boolean {
+public class Basic5Boolean {
 
     /**
      * @param args the command line arguments
