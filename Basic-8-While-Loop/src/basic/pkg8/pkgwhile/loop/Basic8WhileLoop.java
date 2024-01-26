4e9996dd5d25aded54b3507410af6037db9a9c1a
Ufik <ufik0203@gmail.com>
2024-01-26 23:40:18 +0700

    commit

---
+++ Basic-8-While-Loop/src/basic/pkg8/pkgwhile/loop/Basic8WhileLoop.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basic.pkg8.pkgwhile.loop;
+
+/**
+ *
+ * @author mario
+ */
+public class Basic8WhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private void wloop() {
+        boolean a = true;
+        while(a) {
+            
+        }
+    }
+    
+    public static void main(String[] args) {
+        Basic8WhileLoop obj = new Basic8WhileLoop();
+    }
+    
+}
